341748bd7b269943b04c82d62dd0948a4e93dea4
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-04-15 23:02:54 +0700

    fix config

--- libs/config.php
+++ libs/config.php
@@ -1,15 +1,25 @@
 <?php
-    $servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "survey_rg4";
+    // $servername = "localhost";
+    // $username = "root";
+    // $password = "";
+    // $database = "survey_rg4";
 
     // Create connection
-    $conn = new mysqli($servername, $username, $password, $database);
+    // $conn = new mysqli($servername, $username, $password, $database);
     // Check connection
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
-    } 
+    // if ($conn->connect_error) {
+    //     die("Connection failed: " . $conn->connect_error);
+    // } 
+    // mysqli_set_charset($conn,"utf8");
+
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $conn = new mysqli($server, $username, $password, $db);
     mysqli_set_charset($conn,"utf8");
 
     // Create database
