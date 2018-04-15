e7f0fde1ea0a6e7af80244cb9f46f5cb5402d151
Cheevavorn Saitthakool <mean.mea2@gmail.com>
2018-04-15 23:08:38 +0700

    fix This request has been blocked

--- header.php
+++ header.php
@@ -6,11 +6,11 @@
         <link href="https://fonts.googleapis.com/css?family=Pridi" rel="stylesheet">
         <link rel="stylesheet" href="css/themes/purple-theme.css" />
         <link rel="stylesheet" href="css/themes/jquery.mobile.icons.min.css" />
-        <link rel="stylesheet" href="http://code.jquery.com/mobile/1.4.5/jquery.mobile.structure-1.4.5.min.css" /> 
-        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script> 
-        <script src="http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js"></script> 
+        <link rel="stylesheet" href="css/themes/jquery.mobile.structure-1.4.5.min.css" /> 
+        <script src="js/jQuery/jquery-1.11.1.min.js"></script> 
+        <script src="js/jQueryMobile/jquery.mobile-1.4.5.min.js"></script> 
         <script src="js/blockUI/jquery.blockUI.js"></script>
-        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+        <script src="js/sweetalert/sweetalert.min.js"></script>
         <title>Surveys</title>
     </head>
     <body>
